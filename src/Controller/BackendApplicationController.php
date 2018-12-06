c529f66a9a1cd47455d397b01f42fd41138364bd
mxc-commons <frank.hein@maxence.de>
2018-12-06 14:05:34 +0100

    Added finalizeListQuery and finalizeDetailQuery hooks.

--- src/Controller/BackendApplicationController.php
+++ src/Controller/BackendApplicationController.php
@@ -2,6 +2,7 @@
 
 namespace Mxc\Shopware\Plugin\Controller;
 
+use Doctrine\ORM\QueryBuilder;
 use Enlight_Controller_Request_Request;
 use Enlight_Controller_Response_Response;
 use Interop\Container\ContainerInterface;
@@ -34,4 +35,89 @@ class BackendApplicationController extends Shopware_Controllers_Backend_Applicat
         $this->log = $this->services->get('logger');
         parent::__construct($request, $response);
     }
+
+    /**
+     * The getList function returns an array of the configured class model.
+     * The listing query created in the getListQuery function.
+     * The pagination of the listing is handled inside this function.
+     *
+     * @param int   $offset
+     * @param int   $limit
+     * @param array $sort        Contains an array of Ext JS sort conditions
+     * @param array $filter      Contains an array of Ext JS filters
+     * @param array $wholeParams Contains all passed request parameters
+     *
+     * @return array
+     */
+    protected function getList($offset, $limit, $sort = [], $filter = [], array $wholeParams = [])
+    {
+        $builder = $this->getListQuery();
+        $builder->setFirstResult($offset)
+            ->setMaxResults($limit);
+
+        $filter = $this->getFilterConditions(
+            $filter,
+            $this->model,
+            $this->alias,
+            $this->filterFields
+        );
+
+        $sort = $this->getSortConditions(
+            $sort,
+            $this->model,
+            $this->alias,
+            $this->sortFields
+        );
+
+        if (!empty($sort)) {
+            $builder->addOrderBy($sort);
+        }
+
+        if (!empty($filter)) {
+            $builder->addFilter($filter);
+        }
+        $this->finalizeListQuery($builder);
+
+        $paginator = $this->getQueryPaginator($builder);
+        $data = $paginator->getIterator()->getArrayCopy();
+        $count = $paginator->count();
+
+        return ['success' => true, 'data' => $data, 'total' => $count];
+    }
+
+    /**
+     * Contains the logic to get the detailed information of a single record.
+     * The function expects the model identifier value as parameter.
+     * To add additional data to the detailed information you can override the
+     * {@link #getAdditionalDetailData} function.
+     *
+     * To extend the query builder object to select more detailed information,
+     * you can override the {@link #getDetailQuery} function.
+     *
+     * @param int $id - Identifier of the doctrine model
+     *
+     * @return array
+     */
+    public function getDetail($id)
+    {
+        $builder = $this->getDetailQuery($id);
+        $this->finalizeDetailQuery($builder);
+        $paginator = $this->getQueryPaginator($builder);
+        $data = $paginator->getIterator()->current();
+        if (!$data) {
+            $data = [];
+        }
+        $data = $this->getAdditionalDetailData($data);
+
+        return ['success' => true, 'data' => $data];
+    }
+
+    protected function finalizeListQuery(QueryBuilder $builder) {
+        return $builder;
+    }
+
+    protected function finalizeDetailQuery(QueryBuilder $builder) {
+        return $builder;
+    }
+
 }
