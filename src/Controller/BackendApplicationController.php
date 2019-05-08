6113f464ef7d717b23ed8c0f6225f21ce57c6c53
mxc-commons <frank.hein@maxence.de>
2019-05-08 15:41:56 +0200

    Added exception logging for all standard controller actions.

--- src/Controller/BackendApplicationController.php
+++ src/Controller/BackendApplicationController.php
@@ -5,6 +5,7 @@ namespace Mxc\Shopware\Plugin\Controller;
 use Mxc\Shopware\Plugin\Service\LoggerInterface;
 use Mxc\Shopware\Plugin\Service\ServicesTrait;
 use Shopware_Controllers_Backend_Application;
+use Throwable;
 
 class BackendApplicationController extends Shopware_Controllers_Backend_Application
 {
@@ -21,4 +22,81 @@ class BackendApplicationController extends Shopware_Controllers_Backend_Applicat
         }
         return $this->log;
     }
+
+    public function indexAction() {
+        try {
+            parent::indexAction();
+        } catch (Throwable $e) {
+            $this->handleException($e);
+        }
+    }
+
+    public function listAction()
+    {
+        try {
+            parent::listAction();
+        } catch (Throwable $e) {
+            $this->handleException($e);
+        }
+    }
+
+    public function detailAction()
+    {
+        try {
+            parent::listAction();
+        } catch (Throwable $e) {
+            $this->handleException($e);
+        }
+    }
+
+    public function createAction()
+    {
+        try {
+            parent::createAction();
+        } catch (Throwable $e) {
+            $this->handleException($e);
+        }
+    }
+
+    public function updateAction()
+    {
+        try {
+            parent::updateAction();
+        } catch (Throwable $e) {
+            $this->handleException($e);
+        }
+    }
+
+    public function deleteAction()
+    {
+        try {
+            parent::deleteAction();
+        } catch (Throwable $e) {
+            $this->handleException($e);
+        }
+    }
+
+    public function searchAssociationAction()
+    {
+        try {
+            parent::searchAssociationAction();
+        } catch (Throwable $e) {
+            $this->handleException($e);
+        }
+    }
+
+    public function reloadAssociationAction()
+    {
+        try {
+            parent::reloadAssociationAction();
+        } catch (Throwable $e) {
+            $this->handleException($e);
+        }
+    }
+
+    protected function handleException(Throwable $e, bool $rethrow = false) {
+        $this->getLog()->except($e, true, $rethrow);
+        $this->view->assign([ 'success' => false, 'message' => $e->getMessage() ]);
+    }
+
 }
